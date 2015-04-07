855349b1e786b11021950b92c5de28b78c273dc4
Jane Philipps <jane.philipps@gmail.com>
2015-04-06 22:31:26 -0700

    Fixing db

--- week_3/daily_planet/models/article.js
+++ week_3/daily_planet/models/article.js
@@ -3,8 +3,7 @@ module.exports = function(sequelize, DataTypes) {
   var Article = sequelize.define("Article", {
     title: DataTypes.STRING,
     content: DataTypes.TEXT,
-    fiction: DataTypes.BOOLEAN,
-    author_id: DataTypes.INTEGER
+    fiction: DataTypes.BOOLEAN
   }, {
     classMethods: {
       associate: function(models) {
