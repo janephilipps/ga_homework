e9d86dddfad67b001c05e01db5fbfba3d08904c2
Jane Philipps <jane.philipps@gmail.com>
2015-04-23 21:57:24 -0700

    Adding calculator routes and views

--- week_6/calculator_app/app/controllers/calculator_controller.rb
+++ week_6/calculator_app/app/controllers/calculator_controller.rb
@@ -2,4 +2,28 @@ class CalculatorController < ApplicationController
 	def index
 
 	end
+
+	def add
+		x = params[:x].to_i
+		y = params[:y].to_i
+		@sum = x + y
+	end
+
+	def subtract
+		x = params[:x].to_i
+		y = params[:y].to_i
+		@diff = x - y
+	end
+
+	def multiply
+		x = params[:x].to_i
+		y = params[:y].to_i
+		@product = x * y
+	end
+
+	def divide
+		x = params[:x].to_i
+		y = params[:y].to_i
+		@quotient = x / y
+	end
 end
--- week_6/calculator_app/config/routes.rb
+++ week_6/calculator_app/config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   
   root to: 'calculator#index'
 
+  get '/add/:x/:y', to: 'calculator#add'
+
+  get '/subtract/:x/:y', to: 'calculator#subtract'
+
+  get '/multiply/:x/:y', to: 'calculator#multiply'
+
+  get '/divide/:x/:y', to: 'calculator#divide'
+
   end
