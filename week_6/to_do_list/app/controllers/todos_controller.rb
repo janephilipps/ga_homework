0b4b9f68f7fa966c27bba052150d97fa21056c29
Jane Philipps <jane.philipps@gmail.com>
2015-04-25 10:23:49 -0700

    Adding logic to erb files

--- week_6/to_do_list/app/controllers/todos_controller.rb
+++ week_6/to_do_list/app/controllers/todos_controller.rb
@@ -1,9 +1,12 @@
 class TodosController < ApplicationController
 	def index
-		
+		@todos = Todo.all
+		render :index
 	end
 
 	def create
-		
+		todo = params[:todo].permit(:description)
+		Todo.create(todo)
+		redirect_to "/"
 	end
 end
